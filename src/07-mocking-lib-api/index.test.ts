26fee4cd5d440b3230d2c49402060e250d5e6c54
petro1909 <petya-97_97@mail.ru>
2023-06-28 13:52:00 +0300

    feat: add test functions with mocking axios methods

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,50 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+//import axios from 'axios';
+//import lodash from 'lodash';
+//import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+import axios, { AxiosRequestConfig } from 'axios';
 
 describe('throttledGetDataFromApi', () => {
+  const expectedData = { name: 'Mike' };
+  const relativePath = '/testPath';
+  let mockedFunction: jest.SpyInstance<
+    Promise<unknown>,
+    [url: string, config?: AxiosRequestConfig<unknown> | undefined]
+  >;
+  beforeEach(() => {
+    mockedFunction = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementation(async () => ({ data: expectedData }));
+    jest.useFakeTimers();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+    mockedFunction.mockClear();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const createMock = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+
+    const inputURL = createMock.mock.calls[0]?.[0]?.baseURL;
+    const actualURL = createMock.mock.results[0]?.value.defaults.baseURL;
+    expect(inputURL).toEqual(actualURL);
+    createMock.mockClear();
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+
+    const firstCall = mockedFunction.mock.calls[0];
+    expect(firstCall?.[0]).toBe(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const result = await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+    expect(result).toBe(expectedData);
   });
 });
